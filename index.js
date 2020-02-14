0579c69b0cf375d01782bbf1426ee6658846110d
hhamza01 <46751786+hhamza01@users.noreply.github.com>
2020-02-14 13:27:41 +0100

    ok
    ok

---
+++ index.js
@@ -0,0 +1,26 @@
+const  express = require('express');
+const app = express();
+const path = require('path');
+const NewsAPI = require('newsapi');
+var port = process.env.PORT || 3000
+const newsapi = new NewsAPI('4a77e09f6b884fbb93db536ffe8ac917');
+
+let articles;
+newsapi.v2.sources({
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response.sources[0]);
+  articles = response.sources[0];
+});
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render('index', {title: JSON.stringify(articles)["totalResults"],
+     paragraf: JSON.stringify(articles)});
+
+});
+
+app.listen(port);
